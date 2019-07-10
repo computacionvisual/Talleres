706a2482e11dda99ae8ac07b683234bed853e77a
Fabián Camilo Ordóñez <fcordonezo@unal.edu.co>
2019-07-09 23:26:44 -0500

    added greyScale.glsl to videoShaders

---
+++ Taller 4/punto 1/video_shaders/data/greyScale.glsl
@@ -0,0 +1,27 @@
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+
+#define PROCESSING_TEXTURE_SHADER
+
+varying vec4 vertColor;
+varying vec4 vertTexCoord;
+uniform sampler2D texture;
+uniform vec2 texOffset;
+
+void main(void) {
+  // Grouping texcoord variables in order to make it work in the GMA 950. See post #13
+  // in this thread:
+  // http://www.idevgames.com/forums/thread-3467.html
+
+  vec2 tc = vertTexCoord.st + vec2(0.0, 0.0);
+
+  vec4 col = texture2D(texture, tc);
+  float lumaValue = 
+    col.r * 0.2126 + 
+    col.g * 0.7152 + 
+    col.b * 0.0722;
+  vec3 sum = vec3(lumaValue, lumaValue, lumaValue);
+  gl_FragColor = vec4(sum.rgb, 1.0) * vertColor;
+}
